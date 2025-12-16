1192eb4e79e0b38cb0942816e3f48a0df2110342
Carlos Lorenzo <clorenzozuniga@gmail.com>
2025-12-16 12:12:51 +0100

    updated email

--- synaptic/src/components/Connect.tsx
+++ synaptic/src/components/Connect.tsx
@@ -5,7 +5,7 @@ export default function Connect() {
             <p>I believe the most powerful constructs are built through collaboration. Whether you have a complex problem to solve, a shared passion for bio-inspired technology, or a potential opportunity to discuss, my network is always open.</p>
             <h3>Let&apos;s build a new connection.</h3>
             <ul>
-                <li><b>Email // Direct Signal</b>: For all direct inquiries, project discussions, and potential collaborations. This is the most reliable way to reach me. <b>clorenzozuniga@gmail.com</b></li>
+                <li><b>Email // Direct Signal</b>: For all direct inquiries, project discussions, and potential collaborations. This is the most reliable way to reach me. <b>contact@carloslorenzo.dev</b></li>
                 <li><b>LinkedIn // Professional Network</b>: To connect and view my professional profile, career journey, and posts on technology and science. <a href="https://linkedin.com/in/carlos-lorenzo-zúñiga-marí-99a666256" target="_blank">Connect</a></li>
                 <li><b>GitHub // The Blueprints</b>: To explore the source code behind my projects, see my coding style, and track my latest work. <a href="https://github.com/carlos-lorenzo" target="_blank">Explore</a></li>
             </ul>
