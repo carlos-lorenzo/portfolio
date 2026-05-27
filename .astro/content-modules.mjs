98aa61d81925cd1565e21ae4fff2c6895045b5ef
Carlos Lorenzo <clorenzozuniga@gmail.com>
2026-05-27 22:57:18 +0200

    added article on eeg review

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,5 +2,6 @@
 export default new Map([
 ["src/content/blog/cupper-daq.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcupper-daq.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/hh-model.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhh-model.mdx&astroContentModuleFlag=true")],
+["src/content/blog/how-eeg.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-eeg.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/why-snns.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwhy-snns.mdx&astroContentModuleFlag=true")]]);
 		
